77ccbe561f63a55f9ecc74d892ba8e53605d8a37
Ohad Dahan <ohaddahan@gmail.com>
2019-12-17 22:54:14 +0200

    Changed timeout

--- app.ts
+++ app.ts
@@ -27,6 +27,8 @@ app.get('/', function (req: express.Request, res: express.Response) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+let server = app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+server.setTimeout(300000);
